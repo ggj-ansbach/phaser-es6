0cba80c4bd447722df2fdc3e1622af1903581417
Naren Yellavula <naren.yellavula@citrix.com>
2019-01-22 23:44:34 +0100

    first commit

---
+++ src/Game.ts
@@ -0,0 +1,12 @@
+namespace Game {
+  export class MyGame extends Phaser.Game {
+    constructor () {
+      super(800, 600, Phaser.AUTO, 'content', null)
+      this.state.add('Boot', .Boot)
+      this.state.add('Preloader', BasicGame.Preloader)
+      this.state.add('MainMenu', MainMenu, false)
+      this.state.add('Game', BasicGame.Game)
+      this.state.start('Boot')
+    }
+  }
+}
